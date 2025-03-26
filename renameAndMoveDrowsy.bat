f62547111b563b6b3f6b589e9879a6a3aefbaf37
vwani <vinaya_wani@yahoo.com>
2025-03-26 20:49:28 +0530

    initial commit

---
+++ renameAndMoveDrowsy.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Change this to your parent folder path
+set "PARENT_FOLDER=C:\Vinaya\Drowsiness Detection\dataset\images"
+set "NESTED_FOLDER=%PARENT_FOLDER%\Drowsy"
+
+cd /d "%NESTED_FOLDER%"
+
+for %%F in (*) do (
+    set "newname=drowsy-%%F"
+    ren "%%F" "!newname!"
+    move "!newname!" "%PARENT_FOLDER%"
+)
+
+echo Done!
+pause
